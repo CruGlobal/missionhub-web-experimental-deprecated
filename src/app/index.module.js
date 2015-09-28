9bded56160084eb1fd4fd5573c9f0f3aea80f3c8
Scotty Waggoner <ozzieorca@gmail.com>
2015-09-28 13:39:27 -0400

    yo gulp-angular

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('missionhubPrototype', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'restangular', 'ui.router', 'ui.bootstrap']);
+
+})();
