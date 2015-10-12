aa3912fd4b5ca187b190699eade1a9a34f280f7b
Scotty Waggoner <ozzieorca@gmail.com>
2015-10-12 14:01:57 -0400

    Implement adding new email and removing emails. Change make primary btn into radio icon button.

--- src/app/components/people/card/card.directive.js
+++ src/app/components/people/card/card.directive.js
@@ -27,52 +27,73 @@
       vm.showInteractions = true;
       vm.showSurveys = true;
       vm.showMessages = true;
-      vm.person = {
-        fname: 'Scotty',
-        lname: 'Wagggoner',
-        role: 'Intern',
-        gender:'Male',
-        emails: [
-          {
-            type: 'Home',
-            address: 'my.home.email@gmail.com',
-            primary: true
-          },
-          {
-            type: 'Work',
-            address: 'my.work.email@cru.org',
-            primary: false
-          }
-        ],
-        phones: [
-          {
-            type: 'Home',
-            number: '1234567890',
-            primary: true
-          },
-          {
-            type: 'Mobile',
-            number: '0987654321',
-            primary: false
-          }
-        ],
-        addresses: [
-          {
-            type: 'Home',
-            address: '123 Something St',
-            address2: 'Apt 234',
-            city: "My City",
-            state: 'CA',
-            zip: 12345,
-            primary: true
-          }
-        ]
-      };
+      vm.person = getPerson();
+      vm.addEmail = addEmail;
+      vm.removeEmail = removeEmail;
 
       activate();
 
       function activate() {
 
+      }
+
+      function addEmail(){
+        console.log("adding email", vm.newEmail);
+        vm.person.emails.push({
+          type: 'Home',
+          address: vm.newEmail,
+          primary: false,
+          autoFocus: true
+        });
+        vm.newEmail = '';
+      }
+
+      function removeEmail(index){
+        vm.person.emails.splice(index, 1);
+      }
+
+      function getPerson(){
+        return {
+          fname: 'Scotty',
+          lname: 'Wagggoner',
+          role: 'Intern',
+          gender:'Male',
+          emails: [
+            {
+              type: 'Home',
+              address: 'my.home.email@gmail.com',
+              primary: true
+            },
+            {
+              type: 'Work',
+              address: 'my.work.email@cru.org',
+              primary: false
+            }
+          ],
+          phones: [
+            {
+              type: 'Home',
+              number: '1234567890',
+              primary: true
+            },
+            {
+              type: 'Mobile',
+              number: '0987654321',
+              primary: false
+            }
+          ],
+          addresses: [
+            {
+              type: 'Home',
+              address: '123 Something St',
+              address2: 'Apt 234',
+              city: "My City",
+              state: 'CA',
+              zip: 12345,
+              primary: true
+            }
+          ]
+        }
       }
     }
   }
--- src/app/components/people/card/card.module.js
+++ src/app/components/people/card/card.module.js
@@ -2,6 +2,8 @@
   'use strict';
 
   angular
-    .module('missionhub.people.card', []);
+    .module('missionhub.people.card', [
+      'focus-if'
+    ]);
 
 })();
