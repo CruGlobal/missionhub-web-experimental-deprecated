3bc79aff5aef95884da925d42f6e0fc50d9b8017
Scotty Waggoner <ozzieorca@gmail.com>
2015-10-12 16:31:09 -0400

    Add contactInfo directive. Remove floating label from inputs.

---
+++ src/app/components/people/card/contactInfo.directive.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('missionhub.people.card')
+    .directive('contactInfo', contactInfo);
+
+  /** @ngInject */
+  function contactInfo() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'app/components/people/card/contactInfo.html',
+      controller: ContactInfoController,
+      controllerAs: 'contactInfo',
+      bindToController: true,
+      scope: {
+        title: '@',
+        label: '@',
+        data: '=',
+        types: '='
+      }
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function ContactInfoController() {
+      var vm = this;
+      vm.addInfo = addInfo;
+      vm.removeInfo = removeInfo;
+
+      activate();
+
+      function activate() {
+
+      }
+
+      function addInfo(){
+        vm.data.push({
+          type: 'Home',
+          address: vm.newInfo,
+          primary: false,
+          autoFocus: true
+        });
+        vm.newInfo = '';
+      }
+
+      function removeInfo(index){
+        vm.data.splice(index, 1);
+      }
+    }
+  }
+})();
