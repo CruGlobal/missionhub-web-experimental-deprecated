0497eed614f0dbb5226d28ca1364380c74a01580
Scotty Waggoner <ozzieorca@gmail.com>
2015-11-30 11:31:22 -0500

    Move label card into labelCard component.

--- src/app/components/dashboard/dashboard.component.js
+++ src/app/components/dashboard/dashboard.component.js
@@ -11,57 +11,10 @@
   /** @ngInject */
   function DashboardController($mdDialog) {
     var vm = this;
-    vm.showCardDetails = showCardDetails;
 
     activate();
 
     function activate() {
-
-    }
-
-    function showCardDetails(label){
-      $mdDialog.show({
-        templateUrl: 'app/components/dashboard/label.modal.html',
-        controller: LabelModalController,
-        controllerAs: 'labelModal',
-        bindToController: true,
-        parent: angular.element(document.body),
-        clickOutsideToClose:true,
-        locals: {
-          label: label
-        }
-      });
-
-      /** @ngInject */
-      function LabelModalController($mdDialog, $state) {
-        var vm = this;
-        vm.close = close;
-        vm.viewAll = viewAll;
-        vm.goToPerson = goToPerson;
-        vm.sendText = sendText;
-        vm.sendEmail = sendEmail;
-
-        function close() {
-          $mdDialog.hide();
-        }
-
-        function viewAll(){
-          $mdDialog.hide();
-          $state.go('people.index')
-        }
-
-        function goToPerson(person){
-          console.log('navigating to person');
-        }
-
-        function sendEmail(person){
-          console.log('sent email');
-        }
-
-        function sendText(person){
-          console.log('sent text');
-        }
-      }
 
     }
 
---
+++ src/app/components/dashboard/labelCard/labelCard.component.js
@@ -0,0 +1,70 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('missionhub.dashboard')
+    .component('labelCard', {
+      controller: LabelCardController,
+      templateUrl: 'app/components/dashboard/labelCard/labelCard.html',
+      bindings: {
+        data: '='
+      }
+    });
+
+  /** @ngInject */
+  function LabelCardController($mdDialog) {
+    var vm = this;
+    vm.showCardDetails = showCardDetails;
+
+    activate();
+
+    function activate() {
+
+    }
+
+    function showCardDetails(label){
+      $mdDialog.show({
+        templateUrl: 'app/components/dashboard/labelCard/label.modal.html',
+        controller: LabelModalController,
+        controllerAs: 'labelModal',
+        bindToController: true,
+        parent: angular.element(document.body),
+        clickOutsideToClose:true,
+        locals: {
+          label: label
+        }
+      });
+
+      /** @ngInject */
+      function LabelModalController($mdDialog, $state) {
+        var vm = this;
+        vm.close = close;
+        vm.viewAll = viewAll;
+        vm.goToPerson = goToPerson;
+        vm.sendText = sendText;
+        vm.sendEmail = sendEmail;
+
+        function close() {
+          $mdDialog.hide();
+        }
+
+        function viewAll(){
+          $mdDialog.hide();
+          $state.go('people.index')
+        }
+
+        function goToPerson(person){
+          console.log('navigating to person');
+        }
+
+        function sendEmail(person){
+          console.log('sent email');
+        }
+
+        function sendText(person){
+          console.log('sent text');
+        }
+      }
+    }
+  }
+})();
